dd8b90cfbfc81f74609809753a74c76ff8fe1104
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-03-12 17:36:31 +0100

    Stile migliorato con Bootstrap, esercizio completato

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,18 +9,22 @@
         {{-- Includiamo gli assets con la direttiva @vite --}}
         @vite('resources/js/app.js')
     </head>
-    <body>
+    <body class="bg-dark">
 
         <main>
             <div class="container">
                 <div class="row">   
+                    <h1 class="text-center my-4 text-white">Classici in uscita oggi al cinema</h1>
                     @foreach ($movies as $movie)
-                        
-                        <div class="col text-center">
+                        <div class="col-6">
                             
-                            <div class="card">
+                            <div class="card d-flex rounded-0">
                                 <div class="card-body">
-                                    <h1 class="card-title">{{ $movie->title }}</h1>
+                                    <h4 class="d-inline">{{ $movie->id }} - </h4>
+                                    <h4 class="card-title d-inline">{{ $movie->title }}</h4>
+                                    <h5 class="card-title d-inline">({{ $movie->original_title }})</h5>
+                                    <h6 class="card-title text-secondary mt-1">Valutazione media: {{ $movie->vote }}</h6>
+                                    <h6 class="card-title">Data di uscita: {{ $movie->date }}</h6>
                                 </div>
                             </div>
                             
